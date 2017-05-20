e3e601346e8fe2951cba672f54fdd62f007dbd22
Michael Cheers <michaelcheers8@gmail.com>
2017-05-20 17:46:53 +0200

    Added Level Editor (with nothing).

---
+++ JoelFive.LevelEditor/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Bridge;
+using Bridge.Html5;
+
+namespace JoelFive.LevelEditor
+{
+    public class App
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ JoelFive.LevelEditor/JoelFive.LevelEditor.js
@@ -0,0 +1,14 @@
+/**
+ * @version 1.0.0.0
+ * @copyright Copyright ©  2017
+ * @compiler Bridge.NET 16.0.0-beta
+ */
+Bridge.assembly("JoelFive.LevelEditor", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("JoelFive.LevelEditor.App", {
+        main: function Main() {
+
+        }
+    });
+});
